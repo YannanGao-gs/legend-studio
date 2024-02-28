2503b6541f4bc1a6f2eb061bc0264aabde2bf2d2
Yannan <Yannan.Gao@ny.email.gs.com>
2024-02-28 12:25:56 -0500

    Revert fix ag grid type (#2924) This reverts commit a8b4b20211277bd801af40d1622d3d20994281e5.

---
+++ .changeset/popular-tigers-pretend.md
@@ -0,0 +1,7 @@
+---
+'@finos/legend-extension-store-service-store': patch
+'@finos/legend-extension-dsl-diagram': patch
+'@finos/legend-application-studio': patch
+'@finos/legend-query-builder': patch
+'@finos/legend-graph': patch
+---
---
+++ packages/legend-lego/src/data-grid/DataGrid.tsx
@@ -0,0 +1,105 @@
+/**
+ * Copyright (c) 2020-present, Goldman Sachs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  AgGridReact,
+  type AgReactUiProps,
+  type AgGridReactProps,
+} from '@ag-grid-community/react';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { CsvExportModule } from '@ag-grid-community/csv-export';
+import { ClipboardModule } from '@ag-grid-enterprise/clipboard';
+import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
+import { ExcelExportModule } from '@ag-grid-enterprise/excel-export';
+import { FiltersToolPanelModule } from '@ag-grid-enterprise/filter-tool-panel';
+import { MenuModule } from '@ag-grid-enterprise/menu';
+import { RangeSelectionModule } from '@ag-grid-enterprise/range-selection';
+import { RowGroupingModule } from '@ag-grid-enterprise/row-grouping';
+import { ServerSideRowModelModule } from '@ag-grid-enterprise/server-side-row-model';
+import { SideBarModule } from '@ag-grid-enterprise/side-bar';
+import { StatusBarModule } from '@ag-grid-enterprise/status-bar';
+import {
+  type CellRange,
+  type CellMouseOverEvent,
+  type ICellRendererParams,
+  type GridOptions,
+  type ColumnApi,
+  type ColDef,
+  type ColumnState,
+  type GridApi,
+  type IRowNode,
+  type GetContextMenuItemsParams,
+  type MenuItemDef,
+  ModuleRegistry,
+} from '@ag-grid-community/core';
+import { LicenseManager } from '@ag-grid-enterprise/core';
+
+export const communityModules = [ClientSideRowModelModule, CsvExportModule];
+
+export const enterpriseModules = [
+  ClipboardModule,
+  ColumnsToolPanelModule,
+  ExcelExportModule,
+  FiltersToolPanelModule,
+  MenuModule,
+  RangeSelectionModule,
+  RowGroupingModule,
+  ServerSideRowModelModule,
+  SideBarModule,
+  StatusBarModule,
+];
+
+export const allModules = communityModules.concat(enterpriseModules);
+
+declare const AG_GRID_LICENSE: string;
+
+export function DataGrid<TData = unknown>(
+  props: AgGridReactProps<TData> | AgReactUiProps<TData>,
+): JSX.Element {
+  if (AG_GRID_LICENSE) {
+    LicenseManager.setLicenseKey(AG_GRID_LICENSE);
+  }
+  return (
+    <AgGridReact
+      // Temporarily disable usage the browser's ResizeObserver as sometimes, this causes the error
+      // `ResizeObserver loop limit exceeded` when we zoom in too much, in our cases, the problem
+      // seem to arise when the scrollbar visibility changes as row data is being supplied
+      // one way to resolve this problem is to set `alwaysShowVerticalScroll={true}`
+      // See https://github.com/ag-grid/ag-grid/issues/2588
+      suppressBrowserResizeObserver={true}
+      {...props}
+      modules={AG_GRID_LICENSE ? allModules : communityModules}
+    />
+  );
+}
+
+export const configureDataGridComponent = (): void => {
+  ModuleRegistry.registerModules([ClientSideRowModelModule]);
+};
+
+export type {
+  CellRange as DataGridCellRange,
+  CellMouseOverEvent as DataGridCellMouseOverEvent,
+  ICellRendererParams as DataGridCellRendererParams,
+  GridOptions as DataGridOptions,
+  ColDef as DataGridColumnDefinition,
+  ColumnState as DataGridColumnState,
+  ColumnApi as DataGridColumnApi,
+  GridApi as DataGridApi,
+  IRowNode as DataGridIRowNode,
+  GetContextMenuItemsParams as DataGridGetContextMenuItemsParams,
+  MenuItemDef as DataGridMenuItemDef,
+};
